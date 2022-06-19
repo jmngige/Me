d9895d4246e7016810eeca60db375951a4927cf8
jmngige <jamesmngige@gmail.com>
2022-06-19 14:21:58 +0300

    a few ui impls

---
+++ app/src/main/java/com/starsolns/me/views/host/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.starsolns.me.views.host
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.starsolns.me.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
